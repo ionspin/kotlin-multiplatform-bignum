abcb283f8a1c3918285df02481f6c32551ff9d58
Ugljesa Jovanovic <ugljesa.jovanovic@ionspin.com>
2019-03-16 14:50:52 +0100

    Add licence to all files

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -1,19 +1,18 @@
 /*
- * Kontroller
- * Copyright (C) 2018-present Ugljesa Jovanovic (ugljesa.jovanovic@ionspin.com)
+ *    Copyright 2019 Ugljesa Jovanovic
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the Server Side Public License, version 1,
- * as published by MongoDB, Inc.
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * Server Side Public License for more details.
+ *        http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the Server Side Public License
- * along with this program. If not, see
- * <http://www.mongodb.com/licensing/server-side-public-license>.
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
  */
 
 object Versions {
