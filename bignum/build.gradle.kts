9e35ec2d897d38971b123f445041365a89e63f1f
Ugljesa Jovanovic <ugljesa.jovanovic@ionspin.com>
2019-12-01 10:44:11 +0100

    Setting up mingw target

--- bignum/build.gradle.kts
+++ bignum/build.gradle.kts
@@ -95,6 +95,22 @@ kotlin {
         }
     }
 
+    mingwX64() {
+        binaries {
+            staticLib {
+
+            }
+        }
+    }
+
+    mingwX86() {
+        binaries {
+            staticLib {
+
+            }
+        }
+    }
+
     println(targets.names)
 
     sourceSets {
@@ -178,6 +194,22 @@ kotlin {
             dependsOn(nativeMain)
         }
         val linuxTest by getting {
+            dependsOn(nativeTest)
+        }
+
+        val mingwX86Main by getting {
+            dependsOn(nativeMain)
+        }
+
+        val mingwX86Test by getting {
+            dependsOn(nativeTest)
+        }
+
+        val mingwX64Main by getting {
+            dependsOn(nativeMain)
+        }
+
+        val mingwX64Test by getting {
             dependsOn(nativeTest)
         }
     }
