b2450811506bf24e133f9dffc78551c03562034f
Ugljesa Jovanovic <ionspinf@gmail.com>
2020-05-17 21:33:33 +0200

    Kotlin 1.4-M1 bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,7 @@ allprojects {
         mavenCentral()
         google()
         maven("https://kotlin.bintray.com/kotlinx")
+        maven("https://dl.bintray.com/kotlin/kotlin-eap")
         jcenter()
     }
 }
