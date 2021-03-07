da29ad23d513fb099bbc14fe910c83f13442f3a9
Ugljesa Jovanovic <ugljesa.jovanovic@ionspin.com>
2021-03-07 19:40:59 +0100

    Remove main scope as it hangs on ios

--- bignum/src/nativeTest/kotlin/com/ionspin/kotlin/bignum/integer/ConcurrencyTest.kt
+++ bignum/src/nativeTest/kotlin/com/ionspin/kotlin/bignum/integer/ConcurrencyTest.kt
@@ -5,7 +5,6 @@ import kotlin.test.assertTrue
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.newSingleThreadContext
 import kotlinx.coroutines.runBlocking
@@ -24,36 +23,26 @@ class ConcurrencyTest {
      */
     @Test
     fun testDifferentThreads() = runBlocking(Dispatchers.Default) {
-        val mainScope = MainScope()
         val differentThreadScope = CoroutineScope(newSingleThreadContext("DifferentThread"))
         val testTid = pthread_self()
         println("Test thread id $testTid")
-        val mainScopeAsync = mainScope.async {
-            val mainTid = pthread_self()
-            println("Main thread id $mainTid")
-            mainTid
-        }
         val differentThreadAsync = differentThreadScope.async {
             val differentTid = pthread_self()
             println("Different thread id $differentTid")
             differentTid
         }
-        val main = mainScopeAsync.await()
         val different = differentThreadAsync.await()
-        println("Main $main, different $different")
-        assertTrue { main != different }
+        println("Test tid $testTid, different tid $different")
+        assertTrue { testTid != different }
     }
 
     @Test
     fun testSharedAccess() = runBlocking(Dispatchers.Default) {
-        val mainScope = MainScope()
         val differentThreadScope = CoroutineScope(newSingleThreadContext("DifferentThread"))
         val originalBigInt = ULong.MAX_VALUE.toBigInteger()
-        val ubyteArrayAsync = mainScope.async {
-            originalBigInt.toUByteArray()
-        }
+        val ubyteArray = originalBigInt.toUByteArray()
         val reconstructedAsync = differentThreadScope.async {
-            BigInteger.fromUByteArray(ubyteArrayAsync.await(), Sign.POSITIVE)
+            BigInteger.fromUByteArray(ubyteArray, Sign.POSITIVE)
         }
         val reconstructedBigInt = reconstructedAsync.await()
         assertTrue { reconstructedBigInt == originalBigInt }
