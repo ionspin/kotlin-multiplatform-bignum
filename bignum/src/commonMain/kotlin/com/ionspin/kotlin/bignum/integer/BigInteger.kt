8aef7df2dbcf92d6985d973a41340c4514dd4a52
Ugljesa Jovanovic <ugljesa.jovanovic@ionspin.com>
2024-07-14 22:32:42 +0200

    Add TODO

--- bignum/src/commonMain/kotlin/com/ionspin/kotlin/bignum/integer/BigInteger.kt
+++ bignum/src/commonMain/kotlin/com/ionspin/kotlin/bignum/integer/BigInteger.kt
@@ -636,6 +636,7 @@ class BigInteger internal constructor(wordArray: WordArray, requestedSign: Sign)
     }
 
     /** Returns a new BigInt with bits combining [this], [other] doing a bitwise `|`/`or` operation. Forces sign to positive. */
+    // TODO Investigate what is expected behavior when one of the operand is negative. Is it considered to be two's complement?
     override infix fun or(other: BigInteger): BigInteger {
         val resultMagnitude = arithmetic.or(this.magnitude, other.magnitude)
         val resultSign = when {
