e546d32f2b330a4bbfa6572b4f5a3ece2fdc5607
Ugljesa Jovanovic <ugljesa.jovanovic@ionspin.com>
2024-07-15 00:38:40 +0200

    Task ordering, verification, updating targets

--- bignum-serialization-kotlinx/build.gradle.kts
+++ bignum-serialization-kotlinx/build.gradle.kts
@@ -189,6 +189,52 @@ tasks {
             // showStandardStreams = true
         }
     }
+
+    all {
+        // Linux task dependecies
+
+        //@formatter:off
+        if (this.name.equals("signAndroidNativeArm32Publication"))       { this.mustRunAfter("signAndroidNativeArm64Publication") }
+        if (this.name.equals("signAndroidNativeArm64Publication"))       { this.mustRunAfter("signAndroidNativeX64Publication") }
+        if (this.name.equals("signAndroidNativeX64Publication"))         { this.mustRunAfter("signAndroidNativeX86Publication") }
+        if (this.name.equals("signAndroidNativeX86Publication"))         { this.mustRunAfter("signJsPublication") }
+        if (this.name.equals("signJsPublication"))                       { this.mustRunAfter("signJvmPublication") }
+        if (this.name.equals("signJvmPublication"))                      { this.mustRunAfter("signKotlinMultiplatformPublication") }
+        if (this.name.equals("signKotlinMultiplatformPublication"))      { this.mustRunAfter("signLinuxArm64Publication") }
+        if (this.name.equals("signLinuxArm64Publication"))               { this.mustRunAfter("signLinuxX64Publication") }
+        if (this.name.equals("signLinuxX64Publication"))                 { this.mustRunAfter("signMingwX64Publication") }
+        if (this.name.equals("signMingwX64Publication"))                 { this.mustRunAfter("signWasmJsPublication") }
+        if (this.name.equals("signWasmJsPublication"))                   { this.mustRunAfter("signWasmWasiPublication") }
+        //@formatter:on
+
+        if (this.name.startsWith("publish")) {
+            this.mustRunAfter("signAndroidNativeArm32Publication")
+        }
+        // Macos task dependencies
+        // @formatter:off
+        if (this.name.equals("signIosArm64Publication")) { this.mustRunAfter("signIosSimulatorArm64Publication") }
+        if (this.name.equals("signIosSimulatorArm64Publication")) { this.mustRunAfter("signIosX64Publication") }
+        if (this.name.equals("signIosX64Publication")) { this.mustRunAfter("signLinuxArm64Publication") }
+        if (this.name.equals("signLinuxArm64Publication")) { this.mustRunAfter("signLinuxX64Publication") }
+        if (this.name.equals("signLinuxX64Publication")) { this.mustRunAfter("signMacosArm64Publication") }
+        if (this.name.equals("signMacosArm64Publication")) { this.mustRunAfter("signMacosX64Publication") }
+        if (this.name.equals("signMacosX64Publication")) { this.mustRunAfter("signMingwX64Publication") }
+        if (this.name.equals("signMingwX64Publication")) { this.mustRunAfter("signTvosArm64Publication") }
+        if (this.name.equals("signTvosArm64Publication")) { this.mustRunAfter("signTvosSimulatorArm64Publication") }
+        if (this.name.equals("signTvosSimulatorArm64Publication")) { this.mustRunAfter("signTvosX64Publication") }
+        if (this.name.equals("signTvosX64Publication")) { this.mustRunAfter("signWasmJsPublication") }
+        if (this.name.equals("signWasmJsPublication")) { this.mustRunAfter("signWasmWasiPublication") }
+        if (this.name.equals("signWasmWasiPublication")) { this.mustRunAfter("signWatchosArm32Publication") }
+        if (this.name.equals("signWatchosArm32Publication")) { this.mustRunAfter("signWatchosArm64Publication") }
+        if (this.name.equals("signWatchosArm64Publication")) { this.mustRunAfter("signWatchosDeviceArm64Publication") }
+        if (this.name.equals("signWatchosDeviceArm64Publication")) { this.mustRunAfter("signWatchosSimulatorArm64Publication") }
+        if (this.name.equals("signWatchosSimulatorArm64Publication")) { this.mustRunAfter("signWatchosX64Publication") }
+        // @formatter:on
+
+        if (this.name.startsWith("publish")) {
+            this.mustRunAfter("signIosArm64Publication")
+        }
+    }
 }
 
 publishing {
